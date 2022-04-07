63deafafa9116236b04518899ddb88e0c7405003
Enrique <103183567+unclesoju@users.noreply.github.com>
2022-04-07 19:14:47 +0900

    Add files via upload

---
+++ procrastination 1.py
@@ -0,0 +1,8 @@
+name=input("성함이 뭐세요?\n")
+print("반갑습니다, ",name,"씨.",sep="")
+import time
+time.sleep(1.5)
+print(name,", 조박사님이 왜 이렇게 늦죠?!\n")
+time.sleep(1.5)
+location = input("어디사세요, "+name+"씨?\n")
+print("본인 이름은 ",name,". ","피씨엘 직원이고"," 현재 ",location,"에 산다.\n",sep="")
